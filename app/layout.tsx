2009f464471a0d9b7819ed7c100857efa78f3a3e
Amirkaramat <amir_karamat@yahoo.com>
2023-09-24 20:44:17 +0330

    Udate layout.tsx
    add ToastProvider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+import ToasterContext from "./context/ToasterContext";
+
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Realtime Messenger',
-  description: 'Realtime Messenger',
-}
+  title: "Realtime Messenger",
+  description: "Realtime Messenger",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ToasterContext />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
