0c76ba4294a188e12e9e9f26e741787330567938
Amirkaramat <amir_karamat@yahoo.com>
2023-09-24 13:41:58 +0330

    add logo and update root page.tsx

---
+++ app/(site)/page.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div 
+    className="
+    flex
+    min-h-full
+    flex-col
+    justify-center
+    py-12
+    sm:px-6
+    lg:px-8
+    bg-gray-100
+    ">
+      <div 
+      className="
+      sm:mx-auto
+      sm:w-full
+      sm:max-w-md
+      ">
+        <Image
+        src="/images/logo.png"
+        width={48}
+        height={48}
+        className="mx-auto w-auto"
+        alt="logo"
+        />
+        <h2
+        className="
+        mt-6
+        text-center
+        text-3xl
+        font-bold
+        tracking-tight
+        text-gray-900
+        "
+        >
+          Sign in to your account
+        </h2>
+      </div>
+      {/* auth form */}
+    </div>
+  )
+}
