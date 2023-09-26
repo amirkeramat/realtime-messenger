c8b7e384cd386fd75d6029e63a76f2c17d912140
Amirkaramat <amir_karamat@yahoo.com>
2023-09-27 00:34:12 +0330

    Update Sidebar.tsx

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import DesktopSidebar from "./DesktopSidebar";
 import MobileFooter from "./MobileFooter";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 interface SidebarProps {
   children: React.ReactNode;
 }
 
 const Sidebar: React.FC<SidebarProps> = async ({ children }) => {
+
+  const currentUser = await getCurrentUser()
+
   return (
     <div className="h-full">
-      <DesktopSidebar/>
+      <DesktopSidebar currentUser={currentUser!} />
       <MobileFooter/>
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
