68c45ca98639435974a8b9a6e31aea55255acfcf
Amirkaramat <amir_karamat@yahoo.com>
2023-09-26 21:45:22 +0330

    create users page.tsx

---
+++ app/users/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EmptyState from "@/app/components/EmptyState";
+
+const Page = () => {
+  return (
+    <div className="hidden lg:block lg:pl-80 h-full">
+      <EmptyState/>
+    </div>
+  );
+};
+
+export default Page;
