d05f188efc663b6a7d23434f547ec8125e3f2b01
Amirkaramat <amir_karamat@yahoo.com>
2023-09-26 22:51:56 +0330

    Create Sidebar.tsx

---
+++ app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = async ({ children }) => {
+  return (
+    <div className="h-full">
+      <DesktopSidebar/>
+      <MobileFooter/>
+      <main className="lg:pl-20 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default Sidebar;
