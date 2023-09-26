beab6b09b03ce6db685cb2f8a4677fd89478b2ca
Amirkaramat <amir_karamat@yahoo.com>
2023-09-27 00:33:38 +0330

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     swcPlugins: [["next-superjson-plugin", {}]],
   },
+  images: {
+    domains: [
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
 };
 
 module.exports = nextConfig;
