004770a91396b72cef6239d3d8bb26e17d514f6f
Amirkaramat <amir_karamat@yahoo.com>
2023-09-27 11:37:11 +0330

    Update Layout.tsx

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Sidebar from "@/app/components/sidebar/Sidebar";
+import getUsers from "../actions/getUsers";
+import UserList from "./components/UserList";
 
 interface UsersLayoutProps {
   children: React.ReactNode;
 }
 
 const UsersLayout: React.FC<UsersLayoutProps> = async ({ children }) => {
+  const users = await getUsers();
+
   return (
     <Sidebar>
-      <div className=" h-full">{children}</div>
+      <div className=" h-full">
+        <UserList items={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 };
