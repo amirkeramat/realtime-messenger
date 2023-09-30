e76205d6a9100e4e5b1608e01c4857900e60d9e4
Amirkaramat <amir_karamat@yahoo.com>
2023-09-30 11:40:03 +0330

    Update useRoutes.ts

--- app/hooks/useRoutes.ts
+++ app/hooks/useRoutes.ts
@@ -13,9 +13,9 @@ const useRoutes = () => {
     () => [
       {
         label: "Chat",
-        href: "/conversation",
+        href: "/conversations",
         icon: HiChat,
-        active: pathname === "/conversation" || !!conversationId,
+        active: pathname === "/conversations" || !!conversationId,
       },
       {
         label: "Users",
