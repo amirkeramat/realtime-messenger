2862a3edbb3db67d2b95efc4d081300f1bd9bb3c
Amirkaramat <amir_karamat@yahoo.com>
2023-10-01 18:58:16 +0330

    Update app/api/messages/route.tsx
    add pusher to make realtime change in messages

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,6 +1,7 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
+import { pusherServer } from "@/app/libs/pusher";
 export async function POST(request: Request) {
   try {
     const currentUser = await getCurrentUser();
@@ -57,6 +58,18 @@ export async function POST(request: Request) {
           },
         },
       },
+    });
+
+    await pusherServer.trigger(conversationId, "messages:new", newMessage);
+
+    const lastMessage =
+      updatedConversation.messages[updatedConversation.messages.length - 1];
+
+    updatedConversation.users.map((user) => {
+      pusherServer.trigger(user.email!, "conversation:update", {
+        id: conversationId,
+        messages: [lastMessage],
+      });
     });
 
     return NextResponse.json(newMessage);
