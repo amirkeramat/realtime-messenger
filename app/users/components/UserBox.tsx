701090254f06c52ae30816db170f3a683e55f3a7
Amirkaramat <amir_karamat@yahoo.com>
2023-10-01 18:57:00 +0330

    Update UserBox.tsx

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -13,7 +14,6 @@ interface UserBoxProps {
 const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
-  
 
   const handleClick = useCallback(() => {
     setIsLoading(true);
@@ -22,16 +22,19 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
       .post("/api/conversations", { userId: data.id })
       .then((data) => {
         console.log(data);
-        
+
         router.push(`/conversations/${data.data.id}`);
       })
       .finally(() => setIsLoading(false));
   }, [data, router]);
 
   return (
-    <div
-      onClick={handleClick}
-      className="
+    <>
+      {isLoading && <LoadingModal />}
+
+      <div
+        onClick={handleClick}
+        className="
     w-full
     relative
     flex
@@ -44,31 +47,32 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
     transition
     cursor-pointer
     "
-    >
-      <Avatar user={data} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <div
-            className="
+      >
+        <Avatar user={data} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <div
+              className="
                 flex
                 justify-between
                 items-center
                 mb-1
                 "
-          >
-            <p
-              className="
+            >
+              <p
+                className="
                     text-sm
                     font-medium
                     text-gray-900
                     "
-            >
-              {data.name}
-            </p>
+              >
+                {data.name}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
